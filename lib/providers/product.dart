e44e536aade0495fc4cca418643a5417204418b3
triyono <yono.tri@gmail.com>
2020-04-04 22:12:17 +0700

    add favorite func

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -15,4 +15,9 @@ class Product with ChangeNotifier {
       @required this.price,
       @required this.imageUrl,
       this.isFavorite = false});
+
+  void toggleFavoriteStatus() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -32,8 +32,11 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
             color: Theme.of(context).accentColor,
           ),
           title: Text(
