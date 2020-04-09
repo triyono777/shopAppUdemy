00fcafb2ceacba74695095e1b29a5afc068cd1ad
triyono <yono.tri@gmail.com>
2020-04-09 22:49:15 +0700

    change only from left

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CartItem extends StatelessWidget {
@@ -13,22 +14,35 @@ class CartItem extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       background: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.all(20),
       ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {},
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FittedBox(
-                child: Text('$price'),
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FittedBox(
+                  child: Text('$price'),
+                ),
               ),
             ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
