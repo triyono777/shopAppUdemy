f43ae6b20dad58fea954845331729d71c710f484
triyono <yono.tri@gmail.com>
2020-04-11 06:19:13 +0700

    add drawer widget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../widgets/order_item.dart';
 
 class OrderScreen extends StatelessWidget {
+  static const routeName = '/orders';
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
