fbc1154eb6b13238b5649fe200f123b5517c60db
triyono <yono.tri@gmail.com>
2020-05-01 23:06:10 +0700

    add fun patch

--- lib/providers/products_list_provider.dart
+++ lib/providers/products_list_provider.dart
@@ -121,9 +121,20 @@ class ProductsListProvider with ChangeNotifier {
     });
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url =
+          'https://scannen-apps.firebaseio.com/phicosmart/products/$id.json';
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
+
+      //
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -94,7 +94,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
 
     if (_editedProduct.id != null) {
-      Provider.of<ProductsListProvider>(context, listen: false)
+      await Provider.of<ProductsListProvider>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
       setState(() {
         _isLoading = false;
