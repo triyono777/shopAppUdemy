79a57360040cbab7bbb55835a0a4de570a49dfb4
triyono <yono.tri@gmail.com>
2020-05-02 21:58:02 +0700

    add fun delete

--- lib/providers/products_list_provider.dart
+++ lib/providers/products_list_provider.dart
@@ -143,7 +143,17 @@ class ProductsListProvider with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url =
+        'https://scannen-apps.firebaseio.com/phicosmart/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    http.delete(url).then((_) {
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
   }
 }
