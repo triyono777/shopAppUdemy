1752cd1a5be0f779d5f564a2a05bf999c9730ea0
triyono <yono.tri@gmail.com>
2020-05-06 00:00:31 +0700

    refactoring orders creen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -15,15 +15,16 @@ class _OrderScreenState extends State<OrderScreen> {
   var _isLoading = false;
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+//    Future.delayed(Duration.zero).then((_) async {
+
+    _isLoading = true;
+    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
       setState(() {
         _isLoading = false;
       });
     });
+
+//    });
     super.initState();
   }
 
