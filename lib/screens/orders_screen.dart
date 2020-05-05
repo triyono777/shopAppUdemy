f16d84c6b65d838b6aed8b07080b4f703fc3a6ee
triyono <yono.tri@gmail.com>
2020-05-05 23:43:04 +0700

    add loading circular

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -12,10 +12,17 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
+  var _isLoading = false;
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) {
-      Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
     });
     super.initState();
   }
@@ -29,12 +36,16 @@ class _OrderScreenState extends State<OrderScreen> {
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(
-          order: orderData.orders[i],
-        ),
-      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (ctx, i) => OrderItem(
+                order: orderData.orders[i],
+              ),
+            ),
     );
   }
 }
