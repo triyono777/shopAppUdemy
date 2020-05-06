d616a52db4ac9348da3110dcb1c3b8b1eef6d890
triyono <yono.tri@gmail.com>
2020-05-06 23:21:46 +0700

    change metode with future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -27,23 +27,40 @@ class _OrderScreenState extends State<OrderScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('build orders');
+//    final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(
-                order: orderData.orders[i],
-              ),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                // do
+                return Center(
+                  child: Text('An error accured'),
+                );
+              } else {
+                return Consumer<Orders>(
+                  builder: (ctx, orderData, child) => ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, i) => OrderItem(
+                      order: orderData.orders[i],
+                    ),
+                  ),
+                );
+              }
+            }
+          },
+        ));
   }
 }
