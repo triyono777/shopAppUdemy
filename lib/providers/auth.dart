398fa2e729168fa5befd7b5c49974235fc33870e
triyono <yono.tri@gmail.com>
2020-05-09 23:41:08 +0700

    add fun signup

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/widgets.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+
+  Future<void> signup(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB7-boybs7SCE9Q5qIoLB4vNaVbOt4D0rU';
+    http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+  }
 }
