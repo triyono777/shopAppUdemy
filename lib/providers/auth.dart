307c1068a1dffa6290c923705e76dd96b5239a03
triyono <yono.tri@gmail.com>
2020-05-11 10:11:59 +0700

    fixing error print

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,7 +8,7 @@ class Auth with ChangeNotifier {
   String _userId;
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
-    String url =
+    final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB7-boybs7SCE9Q5qIoLB4vNaVbOt4D0rU';
     final response = await http.post(
       url,
@@ -20,7 +20,7 @@ class Auth with ChangeNotifier {
         },
       ),
     );
-    debugPrint(jsonDecode(response.body));
+    print(jsonDecode(response.body));
   }
 
   Future<void> signup(String email, String password) async {
