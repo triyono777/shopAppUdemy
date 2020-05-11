45e0d69d7c139dffa54a5c57534717765beae00e
triyono <yono.tri@gmail.com>
2020-05-11 10:01:23 +0700

    add fun login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -6,6 +6,22 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    String url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB7-boybs7SCE9Q5qIoLB4vNaVbOt4D0rU';
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    debugPrint(jsonDecode(response.body));
+  }
 
   Future<void> signup(String email, String password) async {
     const url =
@@ -21,5 +37,20 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> login(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7-boybs7SCE9Q5qIoLB4vNaVbOt4D0rU';
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
   }
 }
