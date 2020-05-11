4b4e5384e72f278c3f9b99ff61b74e1deafebfc4
triyono <yono.tri@gmail.com>
2020-05-11 10:04:23 +0700

    refactoring

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -24,33 +24,10 @@ class Auth with ChangeNotifier {
   }
 
   Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB7-boybs7SCE9Q5qIoLB4vNaVbOt4D0rU';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7-boybs7SCE9Q5qIoLB4vNaVbOt4D0rU';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
