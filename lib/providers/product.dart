026af0dca53f3f284a44737e685f5b053611b632
triyono <yono.tri@gmail.com>
2020-05-12 22:18:51 +0700

    refactor fun favorite

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -30,10 +30,10 @@ class Product with ChangeNotifier {
     notifyListeners();
 
     try {
-      final response = await http.patch(url,
-          body: json.encode({
-            'isFavorite': isFavorite,
-          }));
+      final response = await http.put(url,
+          body: json.encode(
+            isFavorite,
+          ));
       if (response.statusCode >= 400) {
         _setFavValue(oldStatus);
       }
