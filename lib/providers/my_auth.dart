26405b667eeea22a7e96e3c314bfb65f7692e781
triyono <yono.tri@gmail.com>
2020-05-15 21:52:28 +0700

    add func autologin

--- lib/providers/my_auth.dart
+++ lib/providers/my_auth.dart
@@ -78,6 +78,26 @@ class MyAuth with ChangeNotifier {
     return _authenticate(email, password, 'signInWithPassword');
   }
 
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+
+    final extractedUserData =
+        json.decode(prefs.getString('userData')) as Map<String, Object>;
+    final expiryDate = DateTime.parse(extractedUserData['expireDate']);
+    if (expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    _token = extractedUserData['token'];
+    _userId = extractedUserData['userId'];
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
+  }
+
   void logout() {
     _token = null;
     _userId = null;
